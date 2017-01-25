f7e66362df66381d0f2de552cf3dd9bb35f29a47
Bailey Ward <bailey.wam@gmail.com>
2017-01-25 12:03:00 -0700

    First commit

---
+++ prime.rb
@@ -0,0 +1,26 @@
+require 'prime'
+
+def prime(number)
+  (1..100).each do |num|
+    if num % 5 == 1
+      puts num
+    end
+  end
+end
+
+puts Prime.take(5)
+#puts Prime.take(10)
+
+def find_primes(quantity)
+
+prime = []
+#your code here
+(1..100).each do |num|
+  if num % 5 == 1 &&
+    num % 3 == 1
+  end
+end
+end
+
+puts Prime.take(5)
+#puts Prime.take(3)
---
+++ sorting_suite_pseudo.rb
@@ -0,0 +1,20 @@
+# create an array of numbers
+# count how many numbers are in that array
+# go through each number one by one so we can compare each number to each other.
+# determine if number is bigger or smaller than the one next to it.
+# find a way to swtich them with each other if needed.
+# print out list to double checkx
+
+
+
+
+First - create a method (sort) to take an array of numbers by passing (array)
+as the agrument.
+Second - Then find out how many items are in that array by calling .length and
+setting that equal to a variable so we can call it throughout the program.
+Third - Create a loop for the array that lets us go through each number one by one.
+Do this by using the variable we created (.length) and .times and do.
+Fourth - For each number in the array we want to check if the number we are on is larger
+or smaller than the next number. by using array[i] > array[i]
+Fifth - Find a way to swap them if they need to be.
+Sixth - Print out the final list/array and end program by breaking the loop.
